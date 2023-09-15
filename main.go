7644e771c04958fc9427e13c65c901f6c96be108
Patryk Małek <patryk.malek@konghq.com>
2023-09-15 12:05:12 +0200

    cache

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	_ "github.com/kong/kubernetes-telemetry/pkg/telemetry"
+	_ "github.com/kong/kubernetes-testing-framework/pkg/environments"
+)
 
 func main() {
 	fmt.Println("hello")
